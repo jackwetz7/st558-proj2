ec5b89f50920c851b96a3ed065f210d824a83177
jackwetz7 <jackpretz78@gmail.com>
2024-11-03 12:54:36 -0500

    data subsetting works

--- app.R
+++ app.R
@@ -5,7 +5,7 @@ options(scipen = 999)
 
 house_data <- read_csv("Melbourne_housing_FULL.csv", col_names = TRUE)
 
-# Define UI
+## Define UI
 ui <- fluidPage(
   
   titlePanel("Melbourne Housing Data Exploration!"),
@@ -39,30 +39,91 @@ ui <- fluidPage(
                     "Year the House was Built" = "YearBuilt")
       ),
       
-      uiOutput("slider1")
+      uiOutput("slider1"),
+      
+      selectInput(
+        inputId = "num_var_2",
+        label = "Numerical Variable 2",
+        choices = c("Sale Price" = "Price",
+                    "Number of Rooms" = "Rooms",
+                    "Land Size in Meters" = "Landsize",
+                    "Year the House was Built" = "YearBuilt")
+      ),
+      
+      uiOutput("slider2"),
+      
+      actionButton("sample_data", "Get a Data Sample")
       
     ),
     mainPanel(
+      tableOutput("test_data")
 
     )
   )
 )
 
 
-# Define server logic
+## Define server logic
 server <- function(input, output, session) {
   
+  ## prevent user from choosing the same numeric variables
+  observeEvent(c(input$num_var_1, input$num_var_2), {
+    
+    num_var_1 <- input$num_var_1
+    num_var_2 <- input$num_var_2
+    choices <- c("Sale Price" = "Price",
+                 "Number of Rooms" = "Rooms",
+                 "Land Size in Meters" = "Landsize",
+                 "Year the House was Built" = "YearBuilt")
+    
+    if (num_var_1 == num_var_2){
+      choices <- choices[-which(choices == num_var_1)]
+      updateSelectizeInput(session,
+                           "num_var_2",
+                           choices = choices)
+    }
+  })
+  
+  ## create slider for first numeric variable
   output$slider1 <- renderUI({
     
-    var_range <- range(house_data[[input$num_var_1]], na.rm = TRUE)
+    num_range_1 <- range(house_data[[input$num_var_1]], na.rm = TRUE)
     
-    sliderInput(inputId = "var_range",
+    sliderInput(inputId = "num_range_1",
                 label = paste("Values of", input$num_var_1),
-                min = var_range[1],
-                max = var_range[2],
-                value = var_range)
+                min = num_range_1[1],
+                max = num_range_1[2],
+                value = num_range_1)
   })
+  
+  ## create slider for second numeric variable
+  output$slider2 <- renderUI({
+    
+    num_range_2 <- range(house_data[[input$num_var_2]], na.rm = TRUE)
+    
+    sliderInput(inputId = "num_range_2",
+                label = paste("Values of", input$num_var_2),
+                min = num_range_2[1],
+                max = num_range_2[2],
+                value = num_range_2)
+  })
+  
+  ## create sample from full data set based on user selections
+  house_sample <- reactiveValues()
+  
+  observeEvent(input$sample_data, {
+    
+    house_sample$data <- house_data |>
+      select(input$cat_var, input$num_var_1, input$num_var_2) |>
+      filter(between(!!sym(input$num_var_1), input$num_range_1[1], input$num_range_1[2]),
+             between(!!sym(input$num_var_2), input$num_range_2[1], input$num_range_2[2]))
+  })
+  
+  output$test_data <- renderTable({
+    house_sample$data
+  })
+  
 }
 
-# Run the application 
+## Run the application 
 shinyApp(ui = ui, server = server)
